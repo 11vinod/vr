9e5a3f8952dceff04d2fa656805d53a10f288ec4
11vinod <vinodrathod8221@gmail.com>
2025-06-11 12:08:33 +0530

    Create Ptb1.py

---
+++ Ptb1.py
@@ -0,0 +1,15 @@
+from collections import Counter
+value = input("Enter a value : ")
+if value == value[::-1]:
+print("Palindrome")
+else:
+print("Not Palindrome")
+counted
+_
+dict = Counter(value)
+for key in sorted(counted
+_
+dict.keys()):
+print(f'{key} appears {counted
+_
+dict[key]} times');
