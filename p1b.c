5ebf299a3653e05ff04d587d65755f0524e24639
11vinod <vinodrathod8221@gmail.com>
2025-11-30 09:31:18 +0530

    Delete p1b.c
    Delete

--- p1b.c
+++
@@ -1,9 +0,0 @@
-from collections import Counter
-value = input("Enter a value : ")
-if value == value[::-1]:
-  print("Palindrome")
-else:
-  print("Not Palindrome")
-counted_dict = Counter(value)
-for key in sorted(counted_dict.keys()):
-  print(f'{key} appears {counted_dict[key]} times');
