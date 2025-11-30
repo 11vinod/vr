32e18630e9e41e49633f5650c0ed9d440e2a1852
11vinod <vinodrathod8221@gmail.com>
2025-11-30 09:30:13 +0530

    Delete p1.c
    Delete file

--- p1.c
+++
@@ -1,5 +0,0 @@
-num1 = float(input('Enter first number: '))
-num2 = float(input('Enter second number: '))
-num3 = float(input('Enter third number: '))
-avg = (num1 + num2 + num3)/3
-print('The average of numbers = %0.2f' %avg)
